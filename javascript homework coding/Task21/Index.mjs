7d1366f59382f444dade2c0748ba40185d085f88
SheikhMuzam <115410160+SheikhMuzam@users.noreply.github.com>
2022-11-25 11:14:53 +0500

    Add files via upload

---
+++ javascript homework coding/Task21/Index.mjs
@@ -0,0 +1,10 @@
+// They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.
+
+let personInfo = {
+    id: 2056,
+    name: "Muzammil",
+    skills: "Javascript",
+    country: "Pakistan"
+}
+
+console.log(personInfo);
